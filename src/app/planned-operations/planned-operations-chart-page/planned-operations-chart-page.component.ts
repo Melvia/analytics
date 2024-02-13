3b5bf44b03f7c91bc0a2039e213bfb79f2905d71
Nata <melvia.nr@gmail.com>
2024-02-13 22:59:53 +0500

    fix: add chart.js

--- src/app/planned-operations/planned-operations-chart-page/planned-operations-chart-page.component.ts
+++ src/app/planned-operations/planned-operations-chart-page/planned-operations-chart-page.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Chart} from "chart.js";
 
 @Component({
   selector: 'app-planned-operations-chart-page',
   templateUrl: './planned-operations-chart-page.component.html',
   styleUrl: './planned-operations-chart-page.component.scss'
 })
-export class PlannedOperationsChartPageComponent {
+export class PlannedOperationsChartPageComponent implements OnInit {
+
+  @ViewChild('myChart', { static: true})
+  protected myChart: ElementRef<unknown>;
+ ngOnInit() {
+   const ctx = this.myChart.nativeElement as HTMLCanvasElement;
+
+   const ch = new Chart(ctx, {
+     type: 'bar',
+     data: {
+       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+       datasets: [{
+         label: '# of Votes',
+         data: [12, 19, 3, 5, 2, 3],
+         borderWidth: 1
+       }]
+     },
+     options: {
+       scales: {
+         y: {
+           beginAtZero: true
+         }
+       }
+     }
+   });
+ }
+
 
 }
