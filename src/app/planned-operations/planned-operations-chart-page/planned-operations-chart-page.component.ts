0d93c746373012d6265ff093a53938a061a774a6
rudenko <rudenko@technocom.tech>
2024-02-15 13:58:41 +0500

    feat:change type-diagram

--- src/app/planned-operations/planned-operations-chart-page/planned-operations-chart-page.component.ts
+++ src/app/planned-operations/planned-operations-chart-page/planned-operations-chart-page.component.ts
@@ -18,17 +18,32 @@ export class PlannedOperationsChartPageComponent implements OnInit, AfterViewIni
 
   ngAfterViewInit(): void {
     const ctx = this.myChart.nativeElement as HTMLCanvasElement;
+    const data = {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    };
+
+    const config = {
+      type: 'line',
+      data: data,
+      options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: (ctx) => 'Point Style: ' + ctx.chart.data.datasets[0].pointStyle,
+          }
+        }
+      }
+    };
 
     const ch = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          borderWidth: 1
-        }]
-      },
+      type: 'line',
+      data: data,
       options: {
         scales: {
           y: {
@@ -37,6 +52,8 @@ export class PlannedOperationsChartPageComponent implements OnInit, AfterViewIni
         }
       }
     });
+
+
   }
 
 
