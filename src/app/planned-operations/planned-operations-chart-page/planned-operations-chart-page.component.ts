3c873bab84cd49dbea030d50a48d78173e91140c
rudenko <rudenko@technocom.tech>
2024-02-15 09:23:50 +0500

    feat:chart-example

--- src/app/planned-operations/planned-operations-chart-page/planned-operations-chart-page.component.ts
+++ src/app/planned-operations/planned-operations-chart-page/planned-operations-chart-page.component.ts
@@ -1,37 +1,43 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import {Chart} from "chart.js";
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+// import {Chart} from "chart.js";
+//import { Chart, registerables } from 'chart.js';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-planned-operations-chart-page',
   templateUrl: './planned-operations-chart-page.component.html',
   styleUrl: './planned-operations-chart-page.component.scss'
 })
-export class PlannedOperationsChartPageComponent implements OnInit {
+export class PlannedOperationsChartPageComponent implements OnInit, AfterViewInit {
 
   @ViewChild('myChart', { static: true})
   protected myChart: ElementRef<unknown>;
  ngOnInit() {
-   const ctx = this.myChart.nativeElement as HTMLCanvasElement;
 
-   const ch = new Chart(ctx, {
-     type: 'bar',
-     data: {
-       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-       datasets: [{
-         label: '# of Votes',
-         data: [12, 19, 3, 5, 2, 3],
-         borderWidth: 1
-       }]
-     },
-     options: {
-       scales: {
-         y: {
-           beginAtZero: true
-         }
-       }
-     }
-   });
  }
+
+  ngAfterViewInit(): void {
+    const ctx = this.myChart.nativeElement as HTMLCanvasElement;
+
+    const ch = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 
 
 }
